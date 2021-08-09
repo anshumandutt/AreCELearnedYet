5a59a8e2e13f223ad29856cc5bdd207caa9b20f9
AD <cs.anshumandutt@gmail.com>
2021-08-09 06:32:12 +0000

    fix for env variables

--- lecarb/constants.py
+++ lecarb/constants.py
@@ -1,7 +1,9 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 #import torch
 
+load_dotenv(".env")
 DATA_ROOT = Path(os.environ["DATA_ROOT"])
 OUTPUT_ROOT = Path(os.environ["OUTPUT_ROOT"])
 MODEL_ROOT = OUTPUT_ROOT / "model"
@@ -18,7 +20,8 @@ MYSQL_PSWD = os.environ["MYSQL_PSWD"]
 
 PKL_PROTO = 4
 
-DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+#DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+DEVICE = 'cpu'
 NUM_THREADS = int(os.environ.get("CPU_NUM_THREADS", os.cpu_count()))
 
 VALID_NUM_DATA_DRIVEN = 100
